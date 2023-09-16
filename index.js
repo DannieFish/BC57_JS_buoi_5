9f8cd7585f2af4897b07d5c2da79a33d7a97014c
Duong <nguyenthaiduongdannie@gmail.com>
2023-09-16 21:42:52 +0700

    'added_table'

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById('calculate_score').onclick = function (event) {
+    event.preventDefault()
+
+var reqPoint = +document.getElementById('point_requirement').value;
+
+var subject1 = +document.getElementById('subject_1').value;
+var subject2 = +document.getElementById('subject_2').value;
+var subject3 = +document.getElementById('subject_3').value;
+
+
+
+
+
+}
